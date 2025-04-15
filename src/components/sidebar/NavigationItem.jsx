32befa0fac0694dd71e9fb99e7ce2bc844377400
CREDITMITRAA <developers@creditmitra.in>
2025-04-15 18:36:28 +0530

    shifted Tooltip slightly towards right in SideBar navigation icons

--- src/components/sidebar/NavigationItem.jsx
+++ src/components/sidebar/NavigationItem.jsx
@@ -23,9 +23,9 @@ function NavigationItem({ name, icon, isActive, onClick, showLabel = true }) {
 
       {/* Tooltip - only shows when labels are hidden */}
       {!showLabel && (
-        <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 px-2 py-1 bg-[#EBEBEB] text-[#214768] text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap z-50 pointer-events-none">
+        <div className={`absolute top-full left-1/2 transform -translate-x-1/4 ${isActive ? 'mt-2' : 'mt-0'} px-2 py-1 bg-[#EBEBEB] text-[#214768] text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap z-50 pointer-events-none`}>
           {name}
-          <div className="absolute bottom-full left-1/2 -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-0 border-b-4 border-solid border-l-transparent border-r-transparent border-b-[#EBEBEB]" />
+          <div className="absolute bottom-full left-1/4 -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-0 border-b-4 border-solid border-l-transparent border-r-transparent border-b-[#EBEBEB]" />
         </div>
       )}
     </div>
