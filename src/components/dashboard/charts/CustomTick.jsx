404c89a16fb6f0000db287b2dd8c6ccad6e33491
CREDITMITRAA <developers@creditmitra.in>
2025-04-23 16:29:34 +0530

    handled name length on x axis in graphs

--- src/components/dashboard/charts/CustomTick.jsx
+++ src/components/dashboard/charts/CustomTick.jsx
@@ -1,5 +1,9 @@
+import { truncateWithEllipsis } from "../../../utilities/utility-functions";
+
 function CustomTick({ x, y, payload }){
   const nameParts = payload.value.split(" "); // Split name if needed
+  console.log('payload name graph 1 = ', payload);
+  
   return (
     <g transform={`translate(${x},${y})`}>
       <text
@@ -10,7 +14,7 @@ function CustomTick({ x, y, payload }){
         dx={0} // Ensure it's centered over the bar
         transform="rotate(-60, 0, 0)" // Tilt text for readability
       >
-        <tspan fill="#464646">{payload.value}</tspan>
+        <tspan fill="#464646">{truncateWithEllipsis(payload.value, 12)}</tspan>
         {/* {nameParts.length > 1 && (
           <>
             <tspan fill="#C4C4C4"> - </tspan>
